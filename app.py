b1c9a9bc67d90acb82a5daa2f1353292edfaef9a
ian chen <ianchen12356@gmail.com>
2022-06-16 00:48:33 +0800

    upgrade

--- app.py
+++ app.py
@@ -37,9 +37,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+        r = event.message.text
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='允仲好帥'))
 
 
 if __name__ == "__main__":
